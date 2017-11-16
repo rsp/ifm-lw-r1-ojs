0699381b28bb0b456923088bec7b06eaa0ae3805
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-11-16 20:13:50 +0100

    Add helper functions

---
+++ helpers.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function input1(x) {
+  return function (y) {
+    return x(y);
+  };
+}
+
+function input2(x) {
+  return function (y) {
+    return x(x(y));
+  };
+}
+
+function input3(x) {
+  return function (y) {
+    return x(x(x(x(x(x(x(x(x(x(y))))))))));
+  };
+}
+
+function input4(x) {
+  return input3(input3(x));
+}
+
+function input5(x) {
+  return input3(input4(x));
+}
+
+function inc(x) {
+  return x + 1;
+}
+
+function count(f) {
+  return f(inc)(0);
+}
+
+module.exports = { input1, input2, input3, input4, input5, count };
