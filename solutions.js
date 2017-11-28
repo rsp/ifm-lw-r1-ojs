d365147b7a6c798c121bdd2ef154bfc0d13347fe
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-11-28 13:52:05 +0100

    Use concatenation for compatibility

--- solutions.js
+++ solutions.js
@@ -1,6 +1,6 @@
 'use strict';
 
 for (var i = 1; i <= 3; i++) {
-  var n = `solution-${i}`;
-  module.exports[n] = require(`./${n}`);
+  var n = 'solution-' + i;
+  module.exports[n] = require('./' + n);
 }
